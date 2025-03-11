3bdd8e1e446068727c0ab55d5a5608e9b183f2c5
Jospf Stalin <g31905765.@outlook.com>
2025-03-11 17:44:27 +0800

    Site updated: 2025-03-11 17:44:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/12/KMP/","2024/09/28/ShortNote/","2024/09/30/biSeaNote/","2024/10/17/Tree/","2024/10/09/two-pointer/","2024/09/28/hello-world/"];function toRandomPost(){
+var posts=["2024/10/12/KMP/","2024/09/28/ShortNote/","2024/10/17/Tree/","2024/09/28/hello-world/","2024/09/30/biSeaNote/","2024/10/09/two-pointer/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
